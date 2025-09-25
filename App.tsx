021d2f95b97c055ae8810436c1406c1a2bb1fee2
Mosiah Andrade <mosiahassuncao@GMAIL.COM>
2025-09-25 13:46:26 +0000

    update payload

--- App.tsx
+++ App.tsx
@@ -1,11 +1,20 @@
 
-import React from 'react';
-import Header from './components/Header';
-import Hero from './components/Hero';
-import HowItWorks from './components/HowItWorks';
-import FocusAreas from './components/FocusAreas';
-import ApplicationForm from './components/ApplicationForm';
-import Footer from './components/Footer';
+// import React from 'react';
+// import Header from './components/Header';
+// import Hero from './components/Hero';
+// import HowItWorks from './components/HowItWorks';
+// import FocusAreas from './components/FocusAreas';
+// import ApplicationForm from './components/ApplicationForm';
+// import Footer from './components/Footer';
+import React, { Suspense } from 'react';
+
+// Importa o componente de forma "preguiçosa" (lazy)
+const Header = React.lazy(() => import('./components/Header'));
+const Hero = React.lazy(() => import('./components/Hero'));
+const HowItWorks = React.lazy(() => import('./components/HowItWorks'));
+const FocusAreas = React.lazy(() => import('./components/FocusAreas'));
+const ApplicationForm = React.lazy(() => import('./components/ApplicationForm'));
+const Footer = React.lazy(() => import('./components/Footer'));
 
 const App: React.FC = () => {
   return (
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite';
+import { visualizer } from 'rollup-plugin-visualizer'; 
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
@@ -16,6 +17,7 @@ export default defineConfig(({ mode }) => {
       },
       plugins: [
         tailwindcss(),
+        visualizer({ open: true }),
       ],
     };
 });
